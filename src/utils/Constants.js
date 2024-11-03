ef3288f8e516ec6ca17e8c850e1a74125ffba3b6
Nguyễn Phúc <phucfe050203@gmail.com>
2024-11-03 13:47:27 +0700

    config project

---
+++ src/utils/Constants.js
@@ -0,0 +1,11 @@
+export const APP_ROUTER = {
+    INDEX: "/",
+    HOME: "/home",
+    COMPONENT: "/component",
+    BOOKING: "/booking",
+    AUTH: "auth",
+    LOGIN: "/auth/login",
+    REGISTER: "/auth/register",
+    CHANGE_PASSWORD: "change-password",
+    REFRESH_TOKEN: "/auth/accessToken-generate",
+};
